a24271fb63e83401c8ce1dfa24371592937b2aa8
John Mercedes <mercedes.john@gmail.com>
2020-07-14 20:45:59 -0400

    updates on modules 1 and 2

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,9 +7,9 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
-            
+
 """
-Our doubly-linked list class. It holds references to 
+Our doubly-linked list class. It holds references to
 the list's head and tail nodes.
 """
 class DoublyLinkedList:
@@ -20,15 +20,15 @@ class DoublyLinkedList:
 
     def __len__(self):
         return self.length
-    
+
     """
-    Wraps the given value in a ListNode and inserts it 
-    as the new head of the list. Don't forget to handle 
+    Wraps the given value in a ListNode and inserts it
+    as the new head of the list. Don't forget to handle
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
         pass
-        
+
     """
     Removes the List's current head node, making the
     current head's next node the new head of the List.
@@ -36,47 +36,104 @@ class DoublyLinkedList:
     """
     def remove_from_head(self):
         pass
-            
+
     """
-    Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
+    Wraps the given value in a ListNode and inserts it
+    as the new tail of the list. Don't forget to handle
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
         pass
-            
+
     """
-    Removes the List's current tail node, making the 
+    Removes the List's current tail node, making the
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
         pass
-            
+
     """
-    Removes the input node from its current spot in the 
+    Removes the input node from its current spot in the
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
         pass
-        
+
     """
-    Removes the input node from its current spot in the 
+    Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
         pass
 
     """
-    Deletes the input node from the List, preserving the 
+    Deletes the input node from the List, preserving the
     order of the other elements of the List.
     """
     def delete(self, node):
         pass
 
     """
-    Finds and returns the maximum value of all the nodes 
+    Finds and returns the maximum value of all the nodes
     in the List.
     """
     def get_max(self):
         pass
+
+
+
+
+
+    """
+
+
+
+
+    """
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.head = None
+        self.previous = None
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def traverse(self, starting_point=None):
+        if starting_point is None:
+            starting_point = self.head
+        node = starting_point
+        while node is not None and (node.next != starting_point):
+            yield node
+            node = node.next
+        yield node
+
+    def print_list(self, starting_point=None):
+        nodes = []
+        for node in self.traverse(starting_point):
+            nodes.append(str(node))
+        print(" -> ".join(nodes))
+
+
+
+
+
+if __name__ == "__main__":
+    circular_llist = CircularLinkedList()
+    circular_llist.print_list()
+
+    a = Node('a')
+    b = Node('b')
+    c = Node('c')
+    d = Node('d')
+
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = a
+
+    circular_llist.head = a
+    circular_llist.print_list()
--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -8,13 +8,15 @@ every node has two different fields:
 
  - Next: contains a reference to the next node on the list.
 
- A linked list is therefore a collection of nodes. The first node being the head, and its the starting point for any iteration through the list. While the last node must have a reference pointing to None indicating the end of the list.
+A linked list is therefore a collection of nodes. The first node being the head, and its the starting point for any iteration through the list. While the last node must have a reference pointing to None indicating the end of the list.
 
 """
 
-# first create a class to represet a linked list:
-
 class Node:
+    '''
+    Object representing a node
+
+    '''
     def __init__(self, data):
         self.data = data
         self.next = None
@@ -23,8 +25,110 @@ class Node:
         return self.data
 
 class LinkedList:
-    def __init__(self):
+    '''
+    Object representing a linked list
+
+    '''
+    def __init__(self, nodes=None):
+        # Start of list
         self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
+
+    def __iter__(self):
+        '''
+        Method iterates through the list and yelds every single
+        node. Method always validates that the current node
+        is not None. When this condition is True, it means
+        we've reached the end of the linked list.
+
+        '''
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    # Inserting a new node at the beginning of a list
+    def add_first(self, node):
+        '''
+        Here we are setting self.head as the next reference to the new
+        node so that the new node points to the old self.head.
+        Next we need to state that the new head of the list is the
+        inserted node
+
+        '''
+        node.next = self.head
+        self.head = node
+
+    # Inserting a new node at the end of a list
+    def add_last(self, node):
+        '''
+        Method to insert a new node at the end of the list.
+        This forces us to traverse the whole linked list first
+        and to add the new node when you reach the end.
+
+        '''
+        if not self.head:
+            self.head = node
+            return
+        for current_node in self:
+            pass
+        current_node.next = node
+
+    def add_after(self, target_node_data, new_node):
+        '''
+        Method that adds a node after existing node with a specific value
+
+        '''
+        if not self.head:
+            raise Exception("List is empty")
+
+        for node in self:
+            if node.data == target_node_data:
+                new_node.next = node.next
+                node.next = new_node
+                return
+
+        raise Exception(f'Node with data {target_node_data} is not found')
+
+    def add_before(self, target_node_data, new_mode):
+        if not self.head:
+            raise Exception("List is empty")
+
+        if self.head.data == target_node_data:
+            return self.add_fist(new_mode)
+
+        prev_node = self.head
+        for node in self:
+            if node.data == target_node_data:
+                prev_node.next = new_mode
+                new_mode.next = node
+                return
+            prev_node = node
+
+        raise Exception(f'Node with data {target_node_data} not found')
+
+    def remove_node(self, target_node_data):
+        if not self.head:
+            raise Exception('List is empty')
+
+        if self.head.data == target_node_data:
+            self.head = self.head.next
+            return
+
+        previous_node = self.head
+        for node in self:
+            if node.data == target_node_data:
+                previous_node.next = node.next
+                return
+            previous_node = node
+
+        raise Exception(f'Node with data {target_node_data} not found')
+
 
     def __repr__(self):
         node = self.head
@@ -37,15 +141,28 @@ class LinkedList:
 
 
 if __name__ == '__main__':
-    llist = LinkedList()
-    llist
+    print("Creating a linked list: ")
+    llist = LinkedList(['a', 'b', 'c', 'd', 'e'])
+    print(llist)
 
-    first_node = Node('a')
-    llist.head = first_node
-    llist
+    #for node in llist:
+    #    print(node)
+    print("Inserting 'f' to the first node: ")
+    llist.add_first(Node('f'))
+    print(llist)
 
-    second_node = Node('b')
-    third_node = Node('c')
-    first_node.next = second_node
-    second_node.next = third_node
-    print(llist) # a -> b -> c -> None
+    print("Inserting 'g' to the last node:  ")
+    llist.add_last(Node('g'))
+    print(llist)
+
+    print("Inserting 'h' after 'f': ")
+    llist.add_after('f', Node('h'))
+    print(llist)
+
+    print("Inserting 'i' before 'g': ")
+    llist.add_before('g', Node('i'))
+    print(llist)
+
+    print("Removing 'a' fron node: ")
+    llist.remove_node('a')
+    print(llist)
